dbe2399c488a25a077ed759370015a5928520120
npapoutsakis <nikospapoutsak@gmail.com>
2024-06-19 22:28:18 +0300

    finished 1st phase

--- src/main/scala/SparkAnalytics.scala
+++ src/main/scala/SparkAnalytics.scala
@@ -2,103 +2,101 @@
 //                       Application Scenario 1
 //      Team Members:
 //                  Nikolaos Papoutsakis 2019030206
-//                  Swkratis Siganos     2019030087
+//                  Sokratis Siganos     2019030097
 
 import org.apache.spark.sql.SparkSession
 
 object SparkAnalytics extends App {
 
-  val doc_c_data = "hdfs://localhost:9000/reuters/rcv1-v2.topics.qrels"
-  val doc_t_data = Seq("hdfs://localhost:9000/reuters/lyrl2004_vectors_train.dat",
-    "hdfs://localhost:9000/reuters/lyrl2004_vectors_test_pt0.dat",
-    "hdfs://localhost:9000/reuters/lyrl2004_vectors_test_pt2.dat",
-    "hdfs://localhost:9000/reuters/lyrl2004_vectors_test_pt3.dat",
-    "hdfs://localhost:9000/reuters/lyrl2004_vectors_test_pt1.dat")
-  val stems_data = "hdfs://localhost:9000/reuters/stem.termid.idf.map.txt"
+  val category_file = "hdfs://localhost:9000/reuters/rcv1-v2.topics.qrels"
+  val terms_file = "hdfs://localhost:9000/reuters/lyrl2004_vectors_*.dat"
+  val stems_file = "hdfs://localhost:9000/reuters/stem.termid.idf.map.txt"
 
-  val output = "hdfs://localhost:9000/reuters/output"
+  val categories_out = "hdfs://localhost:9000/reuters/categories_out"
+  val terms_out = "hdfs://localhost:9000/reuters/terms_out"
+  val output = "hdfs://localhost:9000/reuters/JaccardIndex"
+
+  //  val checkpoint_dir = "hdfs://localhost:9000/reuters/checkpoint"
 
   val spark = SparkSession.builder.master("local[*]")
     .appName("Spark Analytics Project")
+    .config("spark.driver.memory", "4g")
+    .config("spark.num.executors", "3")
+    .config("spark.executor.cores", "1")
+    .config("spark.executor.memory", "2g")
     .getOrCreate()
 
-  //***************************************************************************************
-  // DOC(C) Structure - (category_id, [doc1, doc2, doc3, ...]+)
-  val rcv1_v2 = spark.sparkContext.textFile(doc_c_data)
-    .map(line => (line.split(" ")(0), line.split(" ")(1)))    //parsing data (category_id, document_id)
-    .groupByKey()                                             //grouping by key category_id (category_id, [doc1, doc2, doc3, ...]+)
-    .map(x => (x._1, x._2.toSet))                             //2nd arg to set
-
-  // |DOC(C)|
-  val doc_c_result = rcv1_v2.mapValues(_.size)
+  //  spark.sparkContext.setCheckpointDir(checkpoint_dir)
 
   //***************************************************************************************
-  // DOC(T) Structure - (document_id, [term1, term2, term3, ...]+)
-  val terms_per_doc = spark.sparkContext.textFile(doc_t_data.mkString(","))
-    .map(y => {
-      val line = y.split("\\s+")
+  // STEMS
+  val rdd_stems_file = spark.sparkContext.textFile(stems_file)
+    .map(x => {
+      val line = x.split(" ")
+      (line(1), line(0)) // termid stem
+    })
+
+  // CATEGORIES
+  val rdd_categories_file = spark.sparkContext.textFile(category_file)
+    .map(x => {
+      val line = x.split(" ")
+      (line(1), line(0)) //parsing data (docid, catid)
+    })
+
+  val rdd_terms_file = spark.sparkContext.textFile(terms_file)
+    .map(x => {
+      val line = x.split("\\s+")
       val terms = line
         .tail
-        .map(x => x.split(":")(0))
+        .map(y => y.split(":")(0))
       (line.head, terms)
     })
+    .flatMap({
+      case (doc_id, terms) => terms.map(t => (doc_id, t))
+    }) //  docid, termid
 
-  // DOC(T) Structure - (term_id, [doc1, doc2, doc3, ...]+)
-  val docs_per_term = terms_per_doc.flatMap({
-    case (doc_id, terms) => terms.map(t => (t, doc_id))
-  })
-    .groupByKey()
-    .map(x => (x._1, x._2.toSet))
+  val rdd_categories_docs = rdd_categories_file
+    .map(x => (x._2, 1)) // cat, 1
+    .reduceByKey(_+_) // cat, DOC(C)
 
-  // |DOC(T)|- (term_id, number_of_documents)+
-  val doc_t_result = docs_per_term.mapValues(_.size)
+  // TODO: join stems here or later?
+  val rdd_terms_docs = rdd_terms_file
+    .map(x => (x._2, 1))
+    .reduceByKey(_+_) // term, DOC(T)
 
-  //***************************************************************************************
-  // DOC(T)∩DOC(C) - Structure
-  // Broadcast the DOC(C) RDD so it can be used in the map operation efficiently
-  val doc_c_broadcast = spark.sparkContext.broadcast(rcv1_v2.collectAsMap())
+  val rdd_cogrouped = rdd_categories_file.cogroup(rdd_terms_file) // docid , (Seq(cats), Seq(terms))
 
-  // For each term in DOC(T), find the intersection with each category in DOC(C)
-  val intersections = docs_per_term.flatMap {
-      case (term, doc_t_set) =>
-        doc_c_broadcast.value.map {
-          case (category, doc_c_set) =>
-            (term, category, doc_t_set.intersect(doc_c_set))
+  val rdd_intersection = rdd_cogrouped
+    .flatMap({
+      case(docid, (categories, terms)) => {
+        terms.flatMap { t =>
+          categories.map{ c =>
+            ((c,t),1)
+          }
         }
-    }
-
-  //***************************************************************************************
-
-  // Stem_ID Structure - (term_id -> stem_id)
-  val stems = spark.sparkContext.textFile(stems_data)
-    .map(line => {
-      (line.split(" ")(1), line.split(" ")(0))
+      }
     })
-    .collectAsMap()
+    .reduceByKey(_+_) // ((cat, term), intersection)
+    .map(x => (x._1._1, (x._1._2, x._2))) //
+
+  val rdd_joined = rdd_intersection.join(rdd_categories_docs)
+      .map(x => {
+        val (category, ((term, intersection), doc_c)) = x
+        (term, (category, doc_c, intersection))
+      })
+    .join(rdd_terms_docs)
+    .map(x => {
+      val (term, ((category, doc_c, intersection), doc_t)) = x
+      (term, (category, s"${intersection.toFloat / (doc_c + doc_t - intersection)}"))
+    })
+    .join(rdd_stems_file)
+    .map(x => {
+      val (term, ((category, ji), stem)) = x
+      s"$category;$stem;$ji"
+    })
+
+  rdd_joined.saveAsTextFile(output)
 
 
-  //***************************************************************************************
-  // DOC(C) Structure - (category_id, [doc1, doc2, doc3, ...]+)+
-  // DOC(T) Structure - (term_id, [doc1, doc2, doc3, ...]+)+
-  // DOC(T)∩DOC(C) Structure - (category_id_C, term_id_T, category_id_C^term_id_T)+
-  // JaccardIndex Structure (jac_idx(c, t) = category_id_C(intersection)term_id_T/category_id_C(union)term_id_T
-
-  val doc_t_map = doc_t_result.collectAsMap()
-  val doc_c_map = doc_c_result.collectAsMap()
-
-  val jaccard_index = intersections.map({
-    case (term_id, category_id, intersection) => {
-      // need |DOC(T)|, |DOC(C)| and |DOC(T)∩DOC(C)|
-      val numerator = intersection.size
-      val denominator = doc_t_map(term_id) + doc_c_map(category_id) + intersection.size
-      val stem_id = stems(term_id)
-      (s"$category_id;$stem_id;${numerator.toFloat/denominator}")
-    }
-  })
-
-  //save to file hdfs
-  jaccard_index.saveAsTextFile(output)
-  
-  // Terminating Spark Session
   spark.stop()
 }
--- src/main/scala/test.sc
+++
@@ -1,6 +0,0 @@
-val doc_c = ("oikonomia", 2)
-val doc_t = ("trapeza", Seq("2290","2269", "2987", "9290"))
-
-val index = "oikonomia"
-
-val new_doc = Map(doc_c)(index)
